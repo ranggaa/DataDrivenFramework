e8faef8a8ca2f038d81080f7e94f37e383bca396
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-08-09 11:56:20 +0530

    New Class Created

---
+++ DDF_Framework/src/commonFunctions/Demo.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=90,b=87,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=54654,b=8,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void mul()
+{
+	int a=654654,b=7,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
