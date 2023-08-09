f4559bf279fc9a457b3f8da5bfa35a42ceab03a8
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-08-09 11:57:55 +0530

    Modified Values

--- DDF_Framework/src/commonFunctions/Demo.java
+++ DDF_Framework/src/commonFunctions/Demo.java
@@ -3,19 +3,19 @@ package commonFunctions;
 public class Demo {
 public void add()
 {
-	int a=90,b=87,c;
+	int a=56,b=78,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=54654,b=8,c;
+	int a=2345,b=67,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void mul()
 {
-	int a=654654,b=7,c;
+	int a=678,b=70,c;
 	c=a+b;
 	System.out.println(c);
 }
